6c4aa423a09d5b611dd039c608e5d31aea4713d5
神崎航 <kanzwataru@gmail.com>
2019-03-23 11:51:10 -0400

    * split functionality into QuillAnimation standalone class * add simple QuillAnimComponent behaviour for using animation without Animator components

---
+++ Scripts/QuillAnimComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace QuillAnim {
+public class QuillAnimComponent : MonoBehaviour {
+    public int frameRate = 12;
+    private QuillAnimation _animation;
+
+    void Start() {
+        _animation = new QuillAnimation(this.transform, frameRate);
+        _animation.SetFrame(0);
+    }
+
+    void Update() {
+        _animation.Update();
+    }
+    
+    public void Reset() {
+        _animation.SetFrame(0);
+    }
+}
+}
--- Scripts/QuillAnimSystem.cs
+++ Scripts/QuillAnimSystem.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace QuillAnim {
+
+/*
+	Global counter singleton
+ */
 public class QuillAnimSystem : SingletonObject<QuillAnimSystem> {
 	// fps -> millisecond timer
 	List<int> _frameRates = new List<int>(5);
@@ -29,4 +33,78 @@ public class QuillAnimSystem : SingletonObject<QuillAnimSystem> {
 		}
 	}
 }
+
+/*
+	Class that manages Quill FBX animation logic
+	(i.e. switching frame GameObjects on and off and counting frames)
+
+	This is in its own class to allow multiple ways of playing back the animation.
+	(like QuillAnimNode for state machines and QuillAnimComponent for standalone use)
+*/
+public class QuillAnimation {
+	private GameObject[][] _layersFrames;
+    private int _overallFrameCount;
+    private int _currentFrame = 0;
+
+	private int frameRate;
+
+	public QuillAnimation(Transform animation_root, int frameRate) {
+		this.frameRate = frameRate;
+		QuillAnimSystem.instance.RequestFPSCounter(frameRate);
+
+		var root = animation_root.Find("BakedMesh");
+		if(root) {
+			root = root.Find("Root");
+		}
+		else {
+			root = animation_root.Find("Root");
+		}
+
+		int layer_count = root.childCount;
+
+		_layersFrames = new GameObject[layer_count][];  // create arrays for each layer
+		for(int i = 0; i < layer_count; ++i) {          // for each layer, add the frames
+            var layer = root.GetChild(i);
+            
+            var frame_count = layer.childCount;                 // get the number of frames
+            _layersFrames[i] = new GameObject[frame_count];     // create an array for the frames
+            for(int f = 0; f < frame_count; ++f) {              // add all the frames
+                _layersFrames[i][f] = layer.GetChild(f).gameObject;
+                _layersFrames[i][f].SetActive(false);
+            }
+        }
+
+		// take the longest frame as the length of the animation
+        _overallFrameCount = 0;
+        foreach(var layer in _layersFrames) {
+            if(layer.Length > _overallFrameCount)
+                _overallFrameCount = layer.Length;
+        }
+	}
+
+	public void Update() {
+		if(QuillAnimSystem.instance.ShouldUpdate(frameRate)) {
+            int next_frame = _currentFrame + 1;
+            if(next_frame == _overallFrameCount) {
+                next_frame = 0;
+            }
+
+			SetFrame(next_frame);
+		}
+	}
+
+	public void SetFrame(int next_frame) {
+		foreach(var layer in _layersFrames) {
+			if(_currentFrame < layer.Length)
+				layer[_currentFrame].SetActive(false);
+
+			if(next_frame < layer.Length) {
+				layer[next_frame].SetActive(true);
+			}
+		}
+
+		_currentFrame = next_frame;
+	}
+}
+
 }
